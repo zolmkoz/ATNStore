59b63bb000845c1283cc43661c36ddbef2fb0784
Nguyễn Trung Nguyên <49588786+zolmkoz@users.noreply.github.com>
2021-11-10 16:33:10 +0700

    edit

--- Admin/view_orders.php
+++ Admin/view_orders.php
@@ -68,7 +68,7 @@ if(!$_SESSION['admin_username'])
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">EDGE Skateshop - Administrator Panel</a>
+                <a class="navbar-brand" href="index.php">ATN Store - Administrator Panel</a>
             </div>
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
