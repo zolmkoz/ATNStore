34f2d220a7f67428bf82b1e48f3d01b4600a1adf
Nguyễn Trung Nguyên <49588786+zolmkoz@users.noreply.github.com>
2021-11-11 16:20:26 +0700

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -197,25 +197,16 @@ if (!$_SESSION['admin_username']) {
     </div>
 
     <script type="text/javascript" charset="utf-8">
-        $(document).ready(function() {
-            $('#priceinput').keypress(function(event) {
-                return isNumber(event, this)
-            });
-        });
-
+        $(document).ready(function() { $('#priceinput').keypress(function(event) { return isNumber(event, this) }); });
         function isNumber(evt, element) {
-
             var charCode = (evt.which) ? evt.which : event.keyCode
-
             if (
                 (charCode != 45 || $(element).val().indexOf('-') != -1) &&
                 (charCode != 46 || $(element).val().indexOf('.') != -1) &&
                 (charCode < 48 || charCode > 57))
                 return false;
-
             return true;
         }
     </script>
 </body>
-
 </html>
