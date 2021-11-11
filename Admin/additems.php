932101c7e49439c531c2cc1f168c8e99ab3b2f02
Nguyễn Trung Nguyên <49588786+zolmkoz@users.noreply.github.com>
2021-11-11 13:10:07 +0700

    Update additems.php

--- Admin/additems.php
+++ Admin/additems.php
@@ -34,7 +34,7 @@ $imgSize = $_FILES['item_image']['size'];
 $upload_dir = 'item_images/';
 $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); 
 $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); 
-$itempic = rand(1000,1000000).".".$imgExt;
+$itempic = ".".$imgExt;
 
 
 				
