00daf227c3928f2ddbb2c5acdeb2c648121cd653
Nguyễn Trung Nguyên <49588786+zolmkoz@users.noreply.github.com>
2021-11-11 17:31:03 +0700

    Update db_conection.php

--- db_conection.php
+++ db_conection.php
@@ -1,9 +1,7 @@
 <?php
 
+    $$DB_con=mysqli_connect("sql6.freemysqlhosting.net","sql6444323","TL1PB5VrKD");
 
-
-$$DB_con=mysqli_connect("sql6.freemysqlhosting.net","sql6444323","TL1PB5VrKD");
-
-mysqli_select_db($$DB_con,"sql6444323");
+    mysqli_select_db($$DB_con,"sql6444323");
 
 ?>
